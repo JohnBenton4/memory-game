caf7579622f4e34b43a5e1d2ffc83360f82e8c16
John Benton <jtbentoniv@gmail.com>
2022-08-27 07:42:07 -0400

    step 1 complete

--- src/App.js
+++ src/App.js
@@ -1,23 +1,48 @@
-import logo from './logo.svg';
 import './App.css';
+import MemoryCard from './components/MemoryCard';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h1>Memory Game</h1>
+        <h3 class="subtitle">Match Cards To Win</h3>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
         </a>
       </header>
+      <div className="Container">
+        <div class="row1">
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div class="row2">
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div class="row3">
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+        <div class="row4">
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+          <MemoryCard />
+        </div>
+      </div>
+
+
     </div>
   );
 }
